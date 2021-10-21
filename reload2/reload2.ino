5e96bba530573cc775ebaf649193ef72e53517cf
CocoTheOwner <sjoerdvdgoor02@gmail.com>
2021-10-21 15:44:25 +0200

    Final productish?

--- reload2/reload2.ino
+++ reload2/reload2.ino
@@ -1,6 +1,26 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_SleepyDog.h>
 #include <Adafruit_MLX90614.h>
+
+#define SERIAL_ENABLED true
+#define SYM_WATER 37
+#define SYM_TOUCH 48
+#define SYM_DRINK 53
+#define SYM_HOT 65
+#define SYM_DANGR 80
+#define BLINK_TEMP 25
+#define BUZZER_TEMP 50
+#define DEFAULT_MODE 0
+// Mode 0 = default symbol-based warning system
+// Mode 1 = advanced temperature measurement system
+// Mode 2 = off, audio only
+
+
+
+
+
+
+
 #define NOTE_B0  31
 #define NOTE_C1  33
 #define NOTE_CS1 35
@@ -96,33 +116,29 @@ const Adafruit_MLX90614 IR_SENSOR = Adafruit_MLX90614();
 
 void ir_setup() {
 
-  Serial.println("Starting IR setup...");
+  print("Starting IR setup...");
   pinMode(IR_PIN, OUTPUT);
   digitalWrite(IR_PIN, HIGH);
 
   while (!IR_SENSOR.begin()) {
-    Serial.println("Error connecting to MLX sensor. Check wiring.");
+    print("Error connecting to MLX sensor. Check wiring.");
     
     delay(100);
   };
 
-  Serial.println("IR Emissivity = " + String(IR_SENSOR.readEmissivity()));
-  Serial.println("Successively (re)connected to Infrared Sensor");
+  print("IR Emissivity = " + String(IR_SENSOR.readEmissivity()));
+  print("Successively (re)connected to Infrared Sensor");
 }
-
 double ir_get_object_temp() {
   return IR_SENSOR.readObjectTempC();
 }
-
 double ir_get_sensor_temp() {
   return IR_SENSOR.readAmbientTempC();
 }
-
 void ir_enable() {
   digitalWrite(IR_PIN, HIGH);
   setup();
 }
-
 void ir_disable() {
   digitalWrite(IR_PIN, LOW);
 }
@@ -135,6 +151,8 @@ void switch_update() {
   if (CircuitPlayground.slideSwitch()){
     if (!switch_on) {
       switched = true;
+      ir_enable();
+      print("Switched switch on!");
     } else {
       switched = false;
     }
@@ -142,6 +160,9 @@ void switch_update() {
   } else {
     if (switch_on) {
       switched = true;
+      ir_disable();
+      laser_disable();
+      print("Switched switch off!");
     } else {
       switched = false;
     }
@@ -179,84 +200,229 @@ void light_brightness(int brightness){
   CircuitPlayground.strip.setBrightness(brightness);
 }
 
+bool laser_enabled = false;
 void laser_enable() {
-  Serial.println("Enabling laser for " + String(LASER_MAX_MS) + " ms");
-  laser_start = millis();
+  laser_enabled = true;
+  print("Enabling laser");
   digitalWrite(LASER_PIN, HIGH);
 }
-void laser_update() {
-  if (laser_start == -1) return;
-  if (millis() - laser_start > LASER_MAX_MS) {
-    Serial.println("Shutdown laser because timeout");
-    digitalWrite(LASER_PIN, LOW);
-    laser_start = -1;
+void laser_disable() {
+  laser_enabled = false;
+  print("Disabled laser");
+  digitalWrite(LASER_PIN, LOW);
+}
+//void laser_update(bool enable) {
+//  if (enable) {
+//    laser_enable();
+//    return;
+//  }
+//  if (laser_start == -1) return;
+//  if (millis() - laser_start > LASER_MAX_MS) {
+//    print("Shutdown laser because timeout");
+//    digitalWrite(LASER_PIN, LOW);
+//    laser_start = -1;
+//  }
+//}
+
+//#define TAP_EXPIRE_MS 2000
+//#define DTAP_EXPIRE_MS 1500
+//#define TAP_THRESHOLD 30
+//
+//unsigned long tap_tapt = 0;
+//unsigned long tap_dtapt = 0;
+//void tap_tap() { 
+//  print("Tapped! MS " + String(millis()) + " | TAPT " + String(tap_tapt) + " (d: " + String(millis()-tap_tapt) + ") | DTAPT " + String(tap_dtapt));
+//  if (tap_tapt != 0 && millis() - tap_tapt < TAP_EXPIRE_MS) {
+//      tap_tapt = 0;
+//      tap_dtapt = millis();
+//      print("DTap_tapt set to: " + String(tap_dtapt));
+//      return;
+//  }
+//  tap_tapt = millis();
+//  print("Tap_tapt set to: " + String(tap_tapt));
+//}
+//bool tap_tapped() {
+//  if (tap_tapt != 0 && millis() - TAP_EXPIRE_MS < tap_tapt) {
+//    return true;
+//  } else {
+//    tap_tapt = 0;
+//    return false;
+//  }
+//}
+//bool tap_dtapped() {
+//  if (tap_dtapt != 0 && millis() - DTAP_EXPIRE_MS < tap_dtapt) {
+//    return true;
+//  } else {
+//    tap_dtapt = 0;
+//    return false;
+//  }
+//}
+//void tap_clear() {
+//  tap_tapt = 0;
+//  tap_dtapt = 0;
+//}
+
+#define BUTTON_L_PIN 4
+#define BUTTON_R_PIN 19
+bool button_l_on = false;
+bool button_r_on = false;
+
+int main_mode = DEFAULT_MODE;
+bool to_on = false;
+void next_mode() {
+  main_mode = (main_mode + 1) % 3;
+  light_set_all(0, 0, 0);
+  to_on = true;
+}
+#define LIG_WATER 9
+#define LIG_TOUCH 8
+#define LIG_DRINK 7
+#define LIG_HOT 6
+#define LIG_DANGR 5
+
+void do_mode(float t){
+  switch(main_mode){
+    case 0: {
+      if (t > SYM_WATER){
+        light_set(LIG_WATER, 255, 0, 0);
+      } else {
+        light_set(LIG_WATER, 0, 255, 0);
+      }
+      if (t > SYM_TOUCH){
+        light_set(LIG_TOUCH, 255, 0, 0);
+      } else {
+        light_set(LIG_TOUCH, 0, 255, 0);
+      }
+      if (t > SYM_DRINK){
+        light_set(LIG_DRINK, 255, 0, 0);
+      } else {
+        light_set(LIG_DRINK, 0, 255, 0);
+      }
+      if (t > SYM_HOT){
+        light_set(LIG_HOT, 255, 0, 0);
+      } else {
+        light_set(LIG_HOT, 0, 255, 0);
+      }
+      if (t > SYM_DANGR){
+        light_set(LIG_DANGR, 255, 0, 0);
+      } else {
+        light_set(LIG_DANGR, 0, 255, 0);
+      }
+      if (t > BLINK_TEMP && to_on) {
+        to_on = false;
+        for (int i = 0; i < 5; i++){
+          light_set(i, 255, 0, 0);
+        }
+      } else {
+        to_on = true;
+        for (int i = 0; i < 5; i++){
+          light_set(i, 0, 0, 0);
+        }
+      }
+      light_load();
+      break;
+    }
+    case 1: {
+      if (t <= BLINK_TEMP || to_on){
+        for (int i = 0; i < 10; i++) {
+          if (t > -30 + i * 2) {
+            light_set(i, 0, 0, 255);
+          }
+        }
+        for (int i = 0; i < 10; i++) {
+          if (t > -10 + i * 2) {
+            light_set(i, 0, 0, 255);
+          }
+        }
+        for (int i = 0; i < 10; i++) {
+          if (t > 10 + i * 2) {
+            light_set(i, 0, 255, 0);
+          }
+        }
+        for (int i = 0; i < 10; i++) {
+          if (t > 30 + i * 2) {
+            light_set(i, 255, 255, 0);
+          }
+        }
+        for (int i = 0; i < 10; i++) {
+          if (t > 50 + i * 2) {
+            light_set(i, 255, 0, 0);
+          }
+        }
+        for (int i = 0; i < 10; i++) {
+          if (t > 70 + i * 2) {
+            light_set(i, 255, 255, 255);
+          }
+        }
+        light_load();
+        to_on = false;
+      } else {
+        to_on = true;
+        light_set_all(0, 0, 0);
+      }
+      break;
+    }
   }
 }
 
-#define TAP_EXPIRE_MS 2000
-#define DTAP_EXPIRE_MS 1500
-#define TAP_THRESHOLD 30
-
-unsigned long tap_tapt = 0;
-unsigned long tap_dtapt = 0;
-void tap_tap() { 
-  Serial.println("Tapped! MS " + String(millis()) + " | TAPT " + String(tap_tapt) + " (d: " + String(millis()-tap_tapt) + ") | DTAPT " + String(tap_dtapt));
-  if (tap_tapt != 0 && millis() - tap_tapt < TAP_EXPIRE_MS) {
-      tap_tapt = 0;
-      tap_dtapt = millis();
-      Serial.println("DTap_tapt set to: " + String(tap_dtapt));
-      return;
-  }
-  tap_tapt = millis();
-  Serial.println("Tap_tapt set to: " + String(tap_tapt));
-}
-bool tap_tapped() {
-  if (tap_tapt != 0 && millis() - TAP_EXPIRE_MS < tap_tapt) {
-    return true;
+void buttons_update(){
+  if (digitalRead(BUTTON_L_PIN) == HIGH) {
+    if (!button_l_on) {
+      if (laser_enabled){
+        laser_disable();
+      } else {
+        laser_enable();
+      }
+      button_l_on = true;
+      print("Left button pressed!");
+    }
   } else {
-    tap_tapt = 0;
-    return false;
+    if (button_l_on) {
+      print("Left button released!");
+      button_l_on = false;
+    }
   }
-}
-bool tap_dtapped() {
-  if (tap_dtapt != 0 && millis() - DTAP_EXPIRE_MS < tap_dtapt) {
-    return true;
+  if (digitalRead(BUTTON_R_PIN) == HIGH) {
+    if (!button_r_on) {
+      next_mode();
+      button_r_on = true;
+      print("Right button pressed! New mode: " + String(main_mode));
+    }
   } else {
-    tap_dtapt = 0;
-    return false;
+    if (button_r_on) {
+      print("Right button released!");
+      button_r_on = false;
+    }
   }
-}
-void tap_clear() {
-  tap_tapt = 0;
-  tap_dtapt = 0;
 }
 
 void setup() {
-  setup_serial();
+  if (SERIAL_ENABLED){
+    setup_serial();
+  }
   CircuitPlayground.begin();
-  CircuitPlayground.setAccelRange(LIS3DH_RANGE_8_G);
-  CircuitPlayground.setAccelTap(1, TAP_THRESHOLD);
-  attachInterrupt(digitalPinToInterrupt(7), tap_tap, FALLING);
   pinMode(LASER_PIN, OUTPUT);
-  Serial.println("Setup doneish");
+  print("Setup doneish");
+  print("Setup doneish");
   ir_setup();
 }
 
 void loop() {
-  float t = ir_get_object_temp();
-  Serial.println(String(t));
-  for (int i = 0; i < 10; i++){ 
-    if (t - 18 > i) {
-      light_set(i, 64, 64, 64);
-    } else {
-      light_set(i, 0, 0, 0);
-    }
-  }
-  light_load();
-  Serial.print(".");
+  delay(50);
   switch_update();
-//  laser_enable();
-//  laser_update();
+  if (!switch_on){
+    light_set_all(0, 0, 0);
+    laser_disable();
+    delay(950);
+  } else {
+    float t = ir_get_object_temp();
+    if (t > BUZZER_TEMP){
+      CircuitPlayground.playTone(NOTE_D5, 100);
+    }
+    print(String(t));
+    buttons_update();
+    do_mode(t);
+  }
 }
 
 void setup_serial() {
@@ -266,5 +432,7 @@ void setup_serial() {
 }
 
 void print(String s){
-  Serial.println(s);
+  if (SERIAL_ENABLED){
+    Serial.println(s);
+  }
 }
